ae481b60420df2938df9b3ee5511e0d0bdabfe48
Benjamin Kilimnik <benjamin_kilimnik@brown.edu>
2023-07-06 15:32:01 +0200

    update deps to forks, versions

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 import bplib
 
-setup(name='bplib',
+setup(name='bplib_fork',
       version=bplib.VERSION,
       description='A bilinear pairing library for petlib.',
       author='George Danezis',
@@ -16,27 +16,27 @@ setup(name='bplib',
 
       setup_requires=["cffi>=1.0.0",
                       "pytest >= 2.6.4",
-                      "petlib >= 0.0.45"],
-      package_data = {
-            "bplib": ["include/*.h"]},
-      include_package_data = True,
-      tests_require = [
-            "cffi >= 1.0.0",
-            "pycparser >= 2.10",
-            "future >= 0.14.3",
-            "pytest >= 2.5.0",
-            "pytest-cov >= 1.8.1",
-            "petlib >= 0.0.43"
-            ],
+                      "petlib_fork >= 0.0.45"],
+      package_data={
+          "bplib": ["include/*.h"]},
+      include_package_data=True,
+      tests_require=[
+          "cffi >= 1.0.0",
+          "pycparser >= 2.10",
+          "future >= 0.14.3",
+          "pytest >= 2.5.0",
+          "pytest-cov >= 1.8.1",
+          "petlib_fork >= 0.0.43"
+      ],
       cffi_modules=["bplib/compile.py:ffibuilder"],
       install_requires=[
-            "cffi >= 1.0.0",
-            "pycparser >= 2.10",
-            "future >= 0.14.3",
-            "pytest >= 2.5.0",
-            "pytest-cov >= 1.8.1",
-            "msgpack-python >= 0.4.6",
-            "petlib >= 0.0.43"
+          "cffi >= 1.0.0",
+          "pycparser >= 2.10",
+          "future >= 0.14.3",
+          "pytest >= 2.5.0",
+          "pytest-cov >= 1.8.1",
+          "msgpack-python >= 0.4.6",
+          "petlib_fork >= 0.0.43"
       ],
       zip_safe=False,
-)
+      )
